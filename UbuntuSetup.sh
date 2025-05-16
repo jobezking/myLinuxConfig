ffab0e70acaf839a84dd240d149f0db62c23fe9b
jobezking <52982865+jobezking@users.noreply.github.com>
2025-05-16 00:06:30 -0400

    golang

--- UbuntuSetup.sh
+++ UbuntuSetup.sh
@@ -26,7 +26,7 @@ sudo snap install pycharm-community --classic
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
 sudo apt install -y software-properties-common  ca-certificates gnupg lsb-release code git-all gh terraform \
 wget nano vim gnome-console gnome-text-editor python3 python3-pip python3-virtualenv python3-dev build-essential libssl-dev libffi-dev net-tools python3-venv software-properties-common \
-gpg apt-transport-https vlc filezilla openjdk-21-jdk netbeans ./gitkraken-amd64.deb
+gpg apt-transport-https vlc filezilla openjdk-21-jdk netbeans golang-go ./gitkraken-amd64.deb
 
 #Anaconda access  https://repo.anaconda.com/archive and replace below with latest Anaconda3-*-Linux-x86_64.sh
 curl -O https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
