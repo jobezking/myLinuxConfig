00303b0346fece610a59e13475037d31cd86a2b8
jobezking <52982865+jobezking@users.noreply.github.com>
2025-06-09 18:12:41 -0400

    Update docker-jenkins-install.sh

--- docker-jenkins-install.sh
+++ docker-jenkins-install.sh
@@ -1,6 +1,6 @@
 # Docker CE
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common git-all gh -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
