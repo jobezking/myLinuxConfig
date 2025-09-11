4926b62b8de1acbc5ef2482fb090dcd9f2a5d26e
jobezking <52982865+jobezking@users.noreply.github.com>
2025-09-11 13:20:10 -0400

    add docker-ce

--- UbuntuSetup.sh
+++ UbuntuSetup.sh
@@ -5,8 +5,11 @@ sudo apt-get remove --purge 'thunderbird*'
 sudo apt-get autoremove -y
 sudo apt-get clean
 # Add repos for Docker CE
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 #Intellij
 #https://www.jetbrains.com/toolbox-app/download/download-thanks.html?platform=linux
