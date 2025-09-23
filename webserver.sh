1082d3a220ff94081b23ffb916139ec2cde5f871
jobezking <52982865+jobezking@users.noreply.github.com>
2025-09-23 11:38:00 -0400

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -1,7 +1,7 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install apache2 -y
-sudo chown www-data:www-data /var/www/html/test.csv
-sudo chmod -R 644 /var/www/html/test.csv
+#sudo chown www-data:www-data /var/www/html/test.csv
+#sudo chmod -R 644 /var/www/html/test.csv
 sudo systemctl enable apache2
 sudo systemctl status apache2
 sudo ufw allow 'Apache'
