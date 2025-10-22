bdeba0716829800b10a8d284edfc76ff0d1e4c5c
jobezking <52982865+jobezking@users.noreply.github.com>
2025-10-22 13:58:34 -0400

    Update UbuntuSetup.sh

--- UbuntuSetup.sh
+++ UbuntuSetup.sh
@@ -35,13 +35,11 @@ sudo apt update
 
 # for development
 sudo snap install termius-app
-sudo snap install notepad-plus-plus # if the box does not have Steam
-sudo snap install atom --classic #if the box has Steam
 #sudo snap install pycharm-community --classic
 sudo apt install -y code git-all gh konsole wget nano vim gnome-console gnome-text-editor \
 python3 python3-pip python3-virtualenv python3-dev build-essential libssl-dev libffi-dev \
 net-tools python3-venv software-properties-common docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose \
-vlc filezilla default-jdk default-jre netbeans golang-go gnome-boxes microsoft-edge-stable python3.14-full kdenlive 
+vlc filezilla default-jdk default-jre netbeans golang-go gnome-boxes microsoft-edge-stable python3.14-full kate kdenlive 
 
 #Nvidia only
 sudo ubuntu-drivers install; sudo apt install -y nvidia-cuda-toolkit; sudo shutdown -r now
