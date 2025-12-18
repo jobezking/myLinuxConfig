494d1fcab4ef49fd1ffd2e46ad8b06e1439aaf8f
jobezking <52982865+jobezking@users.noreply.github.com>
2025-12-17 22:27:18 -0500

    Add Microsoft Edge repository to Ubuntu setup

--- UbuntuSetup.sh
+++ UbuntuSetup.sh
@@ -31,6 +31,7 @@ sudo add-apt-repository ppa:deadsnakes/ppa  #Python repo
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg  
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge.list'
 rm -f packages.microsoft.gpg
 sudo apt update
 
