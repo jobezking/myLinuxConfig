caae75cf5ec4c719909fff6d9370c22eb1084859
jobezking <52982865+jobezking@users.noreply.github.com>
2026-01-10 15:12:59 -0500

    Install additional packages for NVIDIA toolkit setup

--- mlai/nvidia_cuda.sh
+++ mlai/nvidia_cuda.sh
@@ -94,13 +94,11 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
 # Add NVIDIA container toolkit repo
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
   && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
     sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
-
 sudo apt update
-sudo apt install -y nvidia-container-toolkit
+sudo apt install -y nvidia-container-toolkit software-properties-common docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose
 sudo systemctl restart docker
