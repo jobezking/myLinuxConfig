27b4f84b53eae97188c663b127bf20aed1fd5c17
jobezking <52982865+jobezking@users.noreply.github.com>
2026-01-13 18:55:12 -0500

    Add files via upload

---
+++ mlai/mlai_noGPU.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+conda env list
+#
+conda deactivate; conda create -n py312c13 python=3.12 -c conda-forge -y; \
+conda create -n tf312c13 python=3.12 -c conda-forge -y;
+
+conda activate py312c13
+pip install --upgrade pip setuptools wheel
+pip install paddlepaddle paddleocr \
+    torch torchvision torchaudio \
+    pandas numpy scikit-learn scipy matplotlib matplotlib-inline seaborn xgboost \
+    duckdb tinydb lancedb milvus-lite surrealdb flask python-dotenv flask-wtf \
+    jupyter jupyterlab notebook ipython ipywidgets jupyterlab_widgets ipympl \
+    sqlalchemy requests beautifulsoup4 pillow fsspec pandera pyyaml pip-chill tsfresh \
+    dtale pyjanitor openpyxl statsmodels tqdm itables geopandas \
+    --extra-index-url https://download.pytorch.org/whl/cpu
+conda install -c conda-forge opencv ydata-profiling scikit-bio -y
+conda env export > py312c13.yml
+nvcc --version
+conda deactivate
+
+conda activate tf312c13
+pip install --upgrade pip setuptools wheel
+pip install tensorflow==2.20.0 \
+    paddlepaddle paddleocr \
+    pandas numpy scikit-learn scipy matplotlib matplotlib-inline seaborn xgboost \
+    duckdb tinydb lancedb milvus-lite surrealdb flask python-dotenv flask-wtf \
+    jupyter jupyterlab notebook ipython ipywidgets jupyterlab_widgets ipympl \
+    sqlalchemy requests beautifulsoup4 pillow fsspec pandera pyyaml pip-chill tsfresh \
+    dtale pyjanitor openpyxl statsmodels tqdm itables geopandas
+conda install -c conda-forge opencv ydata-profiling scikit-bio -y
+conda env export > tf312c13.yml
