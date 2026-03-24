3761270d0489c6831dab15ab67216f9edb534141
jobezking <52982865+jobezking@users.noreply.github.com>
2026-03-24 12:53:24 -0400

    Add k8s installation script for dependencies setup

---
+++ k8s/k8s-install.sh
@@ -0,0 +1,2 @@
+sudo apt update; sudo apt upgrade -y; sudo apt dist-upgrade -y; sudo apt autoremove -y
+sudo apt install -y containerd ca-certificates curl gnupg lsb-release vim qemu-guest-agent; sudo systemctl enable --now containerd
