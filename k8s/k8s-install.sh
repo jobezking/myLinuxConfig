f263c826ede2a3aceac151de525ba968c455ec0b
jobezking <52982865+jobezking@users.noreply.github.com>
2026-03-24 18:44:40 -0400

    Update k8s-install.sh for package installation steps

--- k8s/k8s-install.sh
+++ k8s/k8s-install.sh
@@ -1,3 +1,5 @@
+sudo apt update; sudo apt upgrade -y; sudo apt dist-upgrade -y; sudo apt autoremove -y
+sudo apt install -y ca-certificates curl gnupg lsb-release vim
 sudo mkdir -p -m 755 /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.35/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
@@ -7,8 +9,8 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.35/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
-sudo apt update; sudo apt upgrade -y; sudo apt dist-upgrade -y; sudo apt autoremove -y
-sudo apt install -y containerd.io ca-certificates curl gnupg lsb-release vim
+sudo apt update
+sudo apt install -y containerd.io
 containerd config default > config.toml
 sudo mv config.toml /etc/containerd/config.toml
 # Restart to pick up the config
