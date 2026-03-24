4bd3704ff87beebd48fc80e20ea05ee5afb01f41
jobezking <52982865+jobezking@users.noreply.github.com>
2026-03-24 18:41:47 -0400

    Update k8s-install.sh for Docker and Kubernetes keys

--- k8s/k8s-install.sh
+++ k8s/k8s-install.sh
@@ -1,2 +1,15 @@
+sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.35/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.35/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
 sudo apt update; sudo apt upgrade -y; sudo apt dist-upgrade -y; sudo apt autoremove -y
-sudo apt install -y containerd ca-certificates curl gnupg lsb-release vim qemu-guest-agent; sudo systemctl enable --now containerd
+sudo apt install -y containerd.io ca-certificates curl gnupg lsb-release vim
+containerd config default > config.toml
+sudo mv config.toml /etc/containerd/config.toml
+# Restart to pick up the config
+sudo systemctl restart containerd
