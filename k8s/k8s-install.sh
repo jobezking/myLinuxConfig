cb4898318929eda28c64158c9633458d38e065c7
jobezking <52982865+jobezking@users.noreply.github.com>
2026-03-24 18:45:52 -0400

    Refactor keyring setup commands in k8s-install.sh

--- k8s/k8s-install.sh
+++ k8s/k8s-install.sh
@@ -1,8 +1,8 @@
 sudo apt update; sudo apt upgrade -y; sudo apt dist-upgrade -y; sudo apt autoremove -y
 sudo apt install -y ca-certificates curl gnupg lsb-release vim
-sudo mkdir -p -m 755 /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.35/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo mkdir -p -m 755 /etc/apt/keyrings \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg \
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.35/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg \
 sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
