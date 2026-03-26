df4fa98b5d738407a56c99137f1108e2554f0226
jobezking <52982865+jobezking@users.noreply.github.com>
2026-03-26 09:29:42 -0400

    Update k8s-haproxy.sh with HAProxy management commands
    Added commands to restart and check status of HAProxy.

--- k8s/k8s-haproxy.sh
+++ k8s/k8s-haproxy.sh
@@ -2,3 +2,6 @@ sudo systemctl install -y haproxy
 sudo apt install -y haproxy
 sudo systemctl enable haproxy
 upload and edit /etc/haproxy/haproxy.cfg
+sudo haproxy.cfg /etc/haproxy/
+sudo systemctl restart haproxy
+sudo systemctl status haproxy
