0d091a3af60cca31d264f69cfdb56c63da4d7144
jobezking <52982865+jobezking@users.noreply.github.com>
2026-03-26 11:13:15 -0400

    Setup Kubernetes networking and install HAProxy
    Enable necessary kernel modules and configure IP forwarding for Kubernetes. Install and configure HAProxy service.

--- k8s/k8s-haproxy.sh
+++ k8s/k8s-haproxy.sh
@@ -1,3 +1,23 @@
+### enable the bridge netfilter module and flip the IP forwarding switch
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply the changes immediately
+sudo sysctl --system
+
+# install and configure haproxy
+
 sudo systemctl install -y haproxy
 sudo apt install -y haproxy
 sudo systemctl enable haproxy
