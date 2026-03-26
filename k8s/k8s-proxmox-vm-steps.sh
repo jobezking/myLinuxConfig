dda3d37499026a3bcb1b16d4e99710a7f13fca32
jobezking <52982865+jobezking@users.noreply.github.com>
2026-03-26 11:12:10 -0400

    Enable bridge netfilter and IP forwarding
    Added configuration for bridge netfilter and IP forwarding.

--- k8s/k8s-proxmox-vm-steps.sh
+++ k8s/k8s-proxmox-vm-steps.sh
@@ -55,3 +55,20 @@ sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 sudo apt update; sudo apt upgrade -y; sudo apt dist-upgrade -y; sudo apt autoremove -y
 sudo apt install -y qemu-guest-agent ca-certificates curl gnupg lsb-release vim
+### enable the bridge netfilter module and flip the IP forwarding switch
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply the changes immediately
+sudo sysctl --system
